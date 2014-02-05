1c2ab5a35106a46cd88cee4618be904988411207
ludky <ludky109@hotmail.com>
2014-02-05 12:31:14 -0500

    by Fu

---
+++ Proj_usingTemplate/Modroid_App/src/com/example/modroid_app/LoginPage.java
@@ -0,0 +1,37 @@
+package com.example.modroid_app;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+//import android.widget.EditText;
+
+public class LoginPage extends Activity {
+
+	public static final String EXTRA_NAME = "name";
+	
+	
+	//----for M4
+	
+	//private String mName;
+	//private String mPassword;
+
+	// UI references.
+	//private EditText mNameView;
+	//private EditText mPasswordView;
+	
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_login_page);
+		
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.login_page, menu);
+		return true;
+	}
+
+}
