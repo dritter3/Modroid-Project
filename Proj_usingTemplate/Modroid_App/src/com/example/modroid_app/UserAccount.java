e712989e65974476d6cb06dda02bea2fa35ac2c1
ludky <ludky109@hotmail.com>
2014-02-14 08:23:27 -0500

    by Fu

---
+++ Proj_usingTemplate/Modroid_App/src/com/example/modroid_app/UserAccount.java
@@ -0,0 +1,8 @@
+package com.example.modroid_app;
+
+public class UserAccount extends User {
+	public UserAccount(){
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ Proj_usingTemplate/Modroid_App/src/com/example/modroid_app/UserList.java
@@ -0,0 +1,15 @@
+package com.example.modroid_app;
+import java.util.*;
+
+public class UserList {
+
+	private ArrayList<UserAccount> list;
+	
+	public UserList() {
+		// TODO Auto-generated constructor stub
+		list = new ArrayList();
+	}
+	
+	
+
+}
